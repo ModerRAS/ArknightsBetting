ba33c97fb02b14b1dfdb3e4e746ed0546f9df6db
ModerRAS <adqewsx@gmail.com>
2025-04-28 08:40:06 +0800

    添加命令行版和相应的发布

---
+++ ArknightsBetting.Cli/Program.cs
@@ -0,0 +1,18 @@
+﻿using ArknightsBetting.Common;
+
+namespace ArknightsBetting.Cli {
+    internal class Program {
+        static void Main(string[] args) {
+            var list = new List<MainLogic>();
+            var adbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "adb", "adb.exe");
+            foreach (var e in args) {
+                list.Add(new MainLogic(adbPath, e));
+            }
+            var tasks = new List<Task>();
+            foreach (var e in list) {
+                tasks.Add(e.StartGame());
+            }
+            Task.WaitAll(tasks.ToArray());
+        }
+    }
+}
