0daf338bde0930c3a5270d9b4318ae41d975eb63
ModerRAS <adqewsx@gmail.com>
2025-04-28 08:45:05 +0800

    加上日志处理

--- ArknightsBetting.Cli/Program.cs
+++ ArknightsBetting.Cli/Program.cs
@@ -1,8 +1,22 @@
 ﻿using ArknightsBetting.Common;
+using Serilog.Events;
+using Serilog;
 
 namespace ArknightsBetting.Cli {
     internal class Program {
         static void Main(string[] args) {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information() // 设置最低日志级别
+            .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", Serilog.Events.LogEventLevel.Debug) // SQL 语句只在 Debug 级别输出
+            .WriteTo.Console(
+                restrictedToMinimumLevel: LogEventLevel.Information,
+                outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+            .WriteTo.File($"logs/log-.txt",
+              rollingInterval: RollingInterval.Day,
+              outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+            .CreateLogger();
+
+
             var list = new List<MainLogic>();
             var adbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "adb", "adb.exe");
             foreach (var e in args) {
--- ArknightsBetting.Common/MainLogic.cs
+++ ArknightsBetting.Common/MainLogic.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -35,27 +36,33 @@ namespace ArknightsBetting.Common {
                 detect.SetImage(jpg);
                 foreach (var e in detect.GetStringPoints("加入赛事")) {
                     adbWrapper.Tap(e.X, e.Y);
+                    Log.Information($"{saveName}: 加入赛事");
                 }
                 foreach (var e in detect.GetStringPoints("返回主页")) {
                     adbWrapper.Tap(e.X, e.Y);
+                    Log.Information($"{saveName}: 返回主页");
                 }
                 if (detect.Contains("竞猜对决") && detect.Contains("礼物对决") && detect.Contains("自娱自乐")) {
                     foreach (var e in detect.GetStringPoints("自娱自乐")) {
                         adbWrapper.Tap(e.X, e.Y);
+                        Log.Information($"{saveName}: 自娱自乐");
                     }
                     foreach (var e in detect.GetStringPoints("开始")) {
                         adbWrapper.Tap(e.X, e.Y);
+                        Log.Information($"{saveName}: 开始");
                     }
                 }
                 if (detect.Contains("WIN") && detect.Contains("LOSE")) {
                     await File.WriteAllBytesAsync($"Captures/{saveName}/{saveName}_{time.ToString("yyyyMMddHHmmss")}_Result.jpg", jpg);
                     time = DateTime.Now;
+                    Log.Information($"{saveName}: 保存结果");
                     continue;
                 }
                 if (detect.Contains("本轮观望")) {
                     var p1 = detect.GetStringPoint("本轮观望");
                     adbWrapper.Tap(p1.X, p1.Y);
                     await File.WriteAllBytesAsync($"Captures/{saveName}/{saveName}_{time.ToString("yyyyMMddHHmmss")}_Start.jpg", jpg);
+                    Log.Information($"{saveName}: 保存战斗序列");
                     continue;
                 }
             }
